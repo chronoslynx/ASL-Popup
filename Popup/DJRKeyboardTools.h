7367746693170ea21fff7323869c6950c1476d28
Tim Swihart <chronoslynx@gmail.com>
2014-01-28 11:50:13 -0500

    updating gitignore

---
+++ Popup/DJRKeyboardTools.h
@@ -0,0 +1,18 @@
+/* .h */
+
+#import <Foundation/Foundation.h>
+#import <Carbon/Carbon.h>
+
+@interface DJRKeyboardTools : NSObject
+{
+        TISInputSourceRef            layout;
+        const UCKeyboardLayout* layoutData;
+    id                      keyCodeCache;
+}
+
++ (id)sharedInstance;
+- (TISInputSourceRef)keyboardLayout;
+- (NSString *)translateKeyCode:(short)keyCode;
+- (CGKeyCode)keyCodeForChar:(unichar)aChar;
+
+@end
